c1b558f0e113a4585a283f2ce4ca5627eb3fc5b0
joshnwosu <joshuanwosu078@gmail.com>
2022-07-09 00:48:06 +0100

    update

--- app/models/index.js
+++ app/models/index.js
@@ -14,5 +14,18 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
-db.Book = require("./book.model.js")(sequelize, Sequelize);
+db.book = require("./book.model.js")(sequelize, Sequelize);
+db.user = require("./user.model")(sequelize, Sequelize);
+db.role = require("./role.model")(sequelize, Sequelize);
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId",
+});
+db.ROLE = ["user", "admin", "moderator"];
 module.exports = db;
