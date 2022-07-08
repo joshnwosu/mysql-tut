8fed5be3d936a955bd5811a55da50959fb6b5f62
joshnwosu <joshuanwosu078@gmail.com>
2022-07-09 00:51:36 +0100

    update

---
+++ app/routes/book.routes.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+  const books = require("../controllers/book.controller");
+  var router = require("express").Router();
+  // Create a new Tutorial
+  router.post("/", books.create);
+  // Retrieve all Books
+  router.get("/all", books.findAll);
+  // Update a Book with id
+  router.put("/:id", books.update);
+  // Retrieve a sing Book wit id
+  router.get("/:id", books.findOne);
+  // Delete a Book with id
+  router.delete("/:id", books.delete);
+  // Delete all Books
+  router.delete("/", books.deleteAll);
+  app.use("/api/book", router);
+};
